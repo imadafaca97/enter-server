84cb54b00764213428d8edcd66481624ef964f25
Sebastian Santos <sebastian96santos@gmail.com>
2022-05-09 16:53:52 -0400

    get empleado by id

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 @Injectable({})
 export class employeesService {
 
-  async addEmployee(dto) {
+  async addEmployer(dto) {
     const employee = await prisma.empleado.create({
       data: {
         name: dto.name,
@@ -16,8 +16,22 @@ export class employeesService {
     return employee;
   }
   async getall() {
-    const empleados = await prisma.empleado.findMany({});
+    const empleados = await prisma.empleado.findMany({
+      include:{
+        provincia: true,
+        proyectos: true
+      }
+    });
     return empleados;
+  }
+  async getById(dto){
+    console.log(dto)
+    const employee = await prisma.empleado.findFirst({
+      where:{
+        id: dto.id
+      }
+    })
+    return employee
   }
 
   async updateEmployee(dto) {
