1e4d4b6014a7783d43fb598200f3ff908aea7f78
Jeffrey <slassarg20@gmail.com>
2022-05-09 16:09:11 -0400

    Permits modules Created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { provinciasModule } from './provincias/provincias.module';
 import { employeesModule } from './employees/employees.module';
 import { moduleProyecto } from './proyectos/proyecto.module';
 import { usersModule } from './users/users.module';
-
+import { permitssModule } from './permits/permits.module';
 
 @Module({
-  imports: [provinciasModule, employeesModule, moduleProyecto,usersModule],
+  imports: [provinciasModule, employeesModule, moduleProyecto,usersModule,permitssModule],
 })
 export class AppModule {}
