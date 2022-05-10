c3d296a7c253b6e474d17eac0e6ded9b220c81de
Sebastian Santos <sebastian96santos@gmail.com>
2022-05-10 15:12:29 -0400

    endpoint de entrada de empleado

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -4,34 +4,33 @@ const prisma = new PrismaClient();
 
 @Injectable({})
 export class employeesService {
-
   async addEmployer(dto) {
     const employee = await prisma.empleado.create({
       data: {
         name: dto.name,
-        proyectosIds: dto.proyectos,
-        provinciaId: dto.provincia,
+        proyectosIds: dto.project,
+        provinciaId: dto.province,
       },
     });
     return employee;
   }
   async getall() {
     const empleados = await prisma.empleado.findMany({
-      include:{
+      include: {
         provincia: true,
-        proyectos: true
-      }
+        proyectos: true,
+      },
     });
     return empleados;
   }
-  async getById(dto){
-    console.log(dto)
+  async getById(dto) {
+    console.log(dto);
     const employee = await prisma.empleado.findFirst({
-      where:{
-        id: dto.id
-      }
-    })
-    return employee
+      where: {
+        id: dto.id,
+      },
+    });
+    return employee;
   }
 
   async updateEmployee(dto) {
@@ -47,5 +46,23 @@ export class employeesService {
     });
     return employee;
   }
-
+  async employeEntry(dto) {
+    try {
+      let employee = await prisma.empleado.findFirst({
+        where: {
+          id: dto.employee,
+        },
+      });
+      if (employee) {
+        await prisma.employeesEntry.create({
+          data: {
+            employeeID: dto.employee,
+          },
+        });
+        return employee;
+      }
+    } catch (err) {
+      return err;
+    }
+  }
 }
