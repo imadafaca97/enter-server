ed6cc006f113c428d98994b1cf284febb7a4d9d4
Sebastian Santos <sebastian96santos@gmail.com>
2022-06-14 15:53:08 -0400

    visitas agregadas

---
+++ src/visits/visits.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { VisitService } from './visits.service';
+
+@Controller('visits')
+export class VisitsController {
+  constructor(private VisitService: VisitService) {}
+
+  @Post('filterVisit')
+  filterVisits(@Body() dto: any) {
+    return this.VisitService.filterVisits(dto);
+  }
+  @Post('addVisit')
+  addVisit(@Body() dto: any) {
+    return this.VisitService.addVisit(dto);
+  }
+  @Post('editVisit')
+  editVisit(@Body() dto: any) {
+    return this.VisitService.editVisit(dto);
+  }
+  @Post('deleteVisit')
+  deleteVisit(@Body() dto: any) {
+    return this.VisitService.delteVisit(dto);
+  }
+}
